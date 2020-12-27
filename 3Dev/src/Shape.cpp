b19a5443c8b26991504b127e7672b4c5dcc21466
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2020-12-27 19:56:49 +0300

    Add files via upload

--- 3Dev/src/Shape.cpp
+++ 3Dev/src/Shape.cpp
@@ -3,6 +3,7 @@
 Shape::Shape(float w, float h, float d, float x, float y, float z) : w(w), h(h), d(d), x(x), y(y), z(z) {}
 
 void Shape::Draw(GLuint texture) {
+	glTranslatef(x, y, z);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glBegin(GL_QUADS);
 	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, 1.0); glVertex3f(-w, -h, -d);
@@ -36,10 +37,12 @@ void Shape::Draw(GLuint texture) {
 	glTexCoord2f(0, 1); glVertex3f(-w, h, d);
 
 	glEnd();
+	glTranslatef(-x, -y, -z);
 }
 
 void Shape::Draw(GLuint texture[6])
 {
+	glTranslatef(x, y, z);
 	glBindTexture(GL_TEXTURE_2D, texture[0]);
 	glBegin(GL_QUADS);
 	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, 1.0); glVertex3f(-w, -h, -d);
@@ -87,41 +90,5 @@ void Shape::Draw(GLuint texture[6])
 	glTexCoord2f(1, 1); glVertex3f(w, h, d);
 	glTexCoord2f(0, 1); glVertex3f(-w, h, d);
 	glEnd();
-}
-
-void Shape::Draw(sf::Color color)
-{
-	glBegin(GL_QUADS);
-	glColor3f(color.r, color.g, color.b);
-	glNormal3f(0.0, 0.0, 1.0); glVertex3f(-w, -h, -d);
-	glVertex3f(w, -h, -d);
-	glVertex3f(w, h, -d);
-	glVertex3f(-w, h, -d);
-
-	glNormal3f(-1.0, 0.0, 0.0); glVertex3f(d, -h, w);
-	glVertex3f(-w, -h, d);
-	glVertex3f(-w, h, d);
-	glVertex3f(w, h, d);
-
-	glNormal3f(0.0, 0.0, -1.0); glVertex3f(-w, -h, d);
-	glVertex3f(-w, -h, -d);
-	glVertex3f(-w, h, -d);
-	glVertex3f(-w, h, d);
-
-	glNormal3f(1.0, 0.0, 0.0); glVertex3f(w, -h, -d);
-	glVertex3f(w, -h, d);
-	glVertex3f(w, h, d);
-	glVertex3f(w, h, -d);
-
-	glNormal3f(0.0, 1.0, 0.0); glVertex3f(-w, -h, d);
-	glVertex3f(w, -h, d);
-	glVertex3f(w, -h, -d);
-	glVertex3f(-w, -h, -d);
-
-	glNormal3f(0.0, -1.0, 0.0); glVertex3f(-w, h, -d);
-	glVertex3f(w, h, -d);
-	glVertex3f(w, h, d);
-	glVertex3f(-w, h, d);
-
-	glEnd();
+	glTranslatef(-x, -y, -z);
 }
