12d36183ee7b85b08dabaea3a5812842449e2bfd
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-01-31 16:19:39 +0300

    Add files via upload

--- 3Dev/src/Shape.cpp
+++ 3Dev/src/Shape.cpp
@@ -1,94 +1,106 @@
 #include "Shape.h"
 
-Shape::Shape(float w, float h, float d, float x, float y, float z) : w(w), h(h), d(d), x(x), y(y), z(z) {}
+Shape::Shape(float w, float h, float d, float x, float y, float z) : size(w, h, d), position(x, y, z) {}
 
 void Shape::Draw(GLuint texture) {
-	glTranslatef(x, y, z);
+	glPushMatrix();
+	glTranslatef(position.x, position.y, position.z);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, 1.0); glVertex3f(-w, -h, -d);
-	glTexCoord2f(1, 0); glVertex3f(w, -h, -d);
-	glTexCoord2f(1, 1); glVertex3f(w, h, -d);
-	glTexCoord2f(0, 1); glVertex3f(-w, h, -d);
+	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, 1.0); glVertex3f(-size.x, -size.y, -size.z);
+	glTexCoord2f(1, 0); glVertex3f(size.x, -size.y, -size.z);
+	glTexCoord2f(1, 1); glVertex3f(size.x, size.y, -size.z);
+	glTexCoord2f(0, 1); glVertex3f(-size.x, size.y, -size.z);
 
-	glTexCoord2f(0, 0); glNormal3f(-1.0, 0.0, 0.0); glVertex3f(d, -h, w);
-	glTexCoord2f(1, 0); glVertex3f(-w, -h, d);
-	glTexCoord2f(1, 1); glVertex3f(-w, h, d);
-	glTexCoord2f(0, 1); glVertex3f(w, h, d);
+	glTexCoord2f(0, 0); glNormal3f(-1.0, 0.0, 0.0); glVertex3f(size.x, -size.y, size.z);
+	glTexCoord2f(1, 0); glVertex3f(-size.x, -size.y, size.z);
+	glTexCoord2f(1, 1); glVertex3f(-size.x, size.y, size.z);
+	glTexCoord2f(0, 1); glVertex3f(size.x, size.y, size.z);
 
-	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, -1.0); glVertex3f(-w, -h, d);
-	glTexCoord2f(1, 0); glVertex3f(-w, -h, -d);
-	glTexCoord2f(1, 1); glVertex3f(-w, h, -d);
-	glTexCoord2f(0, 1); glVertex3f(-w, h, d);
+	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, -1.0); glVertex3f(-size.x, -size.y, size.z);
+	glTexCoord2f(1, 0); glVertex3f(-size.x, -size.y, -size.z);
+	glTexCoord2f(1, 1); glVertex3f(-size.x, size.y, -size.z);
+	glTexCoord2f(0, 1); glVertex3f(-size.x, size.y, size.z);
 
-	glTexCoord2f(0, 0); glNormal3f(1.0, 0.0, 0.0); glVertex3f(w, -h, -d);
-	glTexCoord2f(1, 0); glVertex3f(w, -h, d);
-	glTexCoord2f(1, 1); glVertex3f(w, h, d);
-	glTexCoord2f(0, 1); glVertex3f(w, h, -d);
+	glTexCoord2f(0, 0); glNormal3f(1.0, 0.0, 0.0); glVertex3f(size.x, -size.y, -size.z);
+	glTexCoord2f(1, 0); glVertex3f(size.x, -size.y, size.z);
+	glTexCoord2f(1, 1); glVertex3f(size.x, size.y, size.z);
+	glTexCoord2f(0, 1); glVertex3f(size.x, size.y, -size.z);
 
-	glTexCoord2f(0, 0); glNormal3f(0.0, 1.0, 0.0); glVertex3f(-w, -h, d);
-	glTexCoord2f(1, 0); glVertex3f(w, -h, d);
-	glTexCoord2f(1, 1); glVertex3f(w, -h, -d);
-	glTexCoord2f(0, 1); glVertex3f(-w, -h, -d);
+	glTexCoord2f(0, 0); glNormal3f(0.0, 1.0, 0.0); glVertex3f(-size.x, -size.y, size.z);
+	glTexCoord2f(1, 0); glVertex3f(size.x, -size.y, size.z);
+	glTexCoord2f(1, 1); glVertex3f(size.x, -size.y, -size.z);
+	glTexCoord2f(0, 1); glVertex3f(-size.x, -size.y, -size.z);
 
-	glTexCoord2f(0, 0); glNormal3f(0.0, -1.0, 0.0); glVertex3f(-w, h, -d);
-	glTexCoord2f(1, 0); glVertex3f(w, h, -d);
-	glTexCoord2f(1, 1); glVertex3f(w, h, d);
-	glTexCoord2f(0, 1); glVertex3f(-w, h, d);
+	glTexCoord2f(0, 0); glNormal3f(0.0, -1.0, 0.0); glVertex3f(-size.x, size.y, -size.z);
+	glTexCoord2f(1, 0); glVertex3f(size.x, size.y, -size.z);
+	glTexCoord2f(1, 1); glVertex3f(size.x, size.y, size.z);
+	glTexCoord2f(0, 1); glVertex3f(-size.x, size.y, size.z);
 
 	glEnd();
-	glTranslatef(-x, -y, -z);
+	glPopMatrix();
 }
 
 void Shape::Draw(GLuint texture[6])
 {
-	glTranslatef(x, y, z);
+	glPushMatrix();
+	glTranslatef(position.x, position.y, position.z);
 	glBindTexture(GL_TEXTURE_2D, texture[0]);
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, 1.0); glVertex3f(-w, -h, -d);
-	glTexCoord2f(1, 0); glVertex3f(w, -h, -d);
-	glTexCoord2f(1, 1); glVertex3f(w, h, -d);
-	glTexCoord2f(0, 1); glVertex3f(-w, h, -d);
+	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, 1.0); glVertex3f(-size.x, -size.y, -size.z);
+	glTexCoord2f(1, 0); glVertex3f(size.x, -size.y, -size.z);
+	glTexCoord2f(1, 1); glVertex3f(size.x, size.y, -size.z);
+	glTexCoord2f(0, 1); glVertex3f(-size.x, size.y, -size.z);
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, texture[1]);
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0); glNormal3f(-1.0, 0.0, 0.0); glVertex3f(d, -h, w);
-	glTexCoord2f(1, 0); glVertex3f(-w, -h, d);
-	glTexCoord2f(1, 1); glVertex3f(-w, h, d);
-	glTexCoord2f(0, 1); glVertex3f(w, h, d);
+	glTexCoord2f(0, 0); glNormal3f(-1.0, 0.0, 0.0); glVertex3f(size.x, -size.y, size.z);
+	glTexCoord2f(1, 0); glVertex3f(-size.x, -size.y, size.z);
+	glTexCoord2f(1, 1); glVertex3f(-size.x, size.y, size.z);
+	glTexCoord2f(0, 1); glVertex3f(size.x, size.y, size.z);
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, texture[2]);
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, -1.0); glVertex3f(-w, -h, d);
-	glTexCoord2f(1, 0); glVertex3f(-w, -h, -d);
-	glTexCoord2f(1, 1); glVertex3f(-w, h, -d);
-	glTexCoord2f(0, 1); glVertex3f(-w, h, d);
+	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, -1.0); glVertex3f(-size.x, -size.y, size.z);
+	glTexCoord2f(1, 0); glVertex3f(-size.x, -size.y, -size.z);
+	glTexCoord2f(1, 1); glVertex3f(-size.x, size.y, -size.z);
+	glTexCoord2f(0, 1); glVertex3f(-size.x, size.y, size.z);
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, texture[3]);
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0); glNormal3f(1.0, 0.0, 0.0); glVertex3f(w, -h, -d);
-	glTexCoord2f(1, 0); glVertex3f(w, -h, d);
-	glTexCoord2f(1, 1); glVertex3f(w, h, d);
-	glTexCoord2f(0, 1); glVertex3f(w, h, -d);
+	glTexCoord2f(0, 0); glNormal3f(1.0, 0.0, 0.0); glVertex3f(size.x, -size.y, -size.z);
+	glTexCoord2f(1, 0); glVertex3f(size.x, -size.y, size.z);
+	glTexCoord2f(1, 1); glVertex3f(size.x, size.y, size.z);
+	glTexCoord2f(0, 1); glVertex3f(size.x, size.y, -size.z);
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, texture[4]);
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0); glNormal3f(0.0, 1.0, 0.0); glVertex3f(-w, -h, d);
-	glTexCoord2f(1, 0); glVertex3f(w, -h, d);
-	glTexCoord2f(1, 1); glVertex3f(w, -h, -d);
-	glTexCoord2f(0, 1); glVertex3f(-w, -h, -d);
+	glTexCoord2f(0, 0); glNormal3f(0.0, 1.0, 0.0); glVertex3f(-size.x, -size.y, size.z);
+	glTexCoord2f(1, 0); glVertex3f(size.x, -size.y, size.z);
+	glTexCoord2f(1, 1); glVertex3f(size.x, -size.y, -size.z);
+	glTexCoord2f(0, 1); glVertex3f(-size.x, -size.y, -size.z);
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, texture[5]);
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0); glNormal3f(0.0, -1.0, 0.0); glVertex3f(-w, h, -d);
-	glTexCoord2f(1, 0); glVertex3f(w, h, -d);
-	glTexCoord2f(1, 1); glVertex3f(w, h, d);
-	glTexCoord2f(0, 1); glVertex3f(-w, h, d);
+	glTexCoord2f(0, 0); glNormal3f(0.0, -1.0, 0.0); glVertex3f(-size.x, size.y, -size.z);
+	glTexCoord2f(1, 0); glVertex3f(size.x, size.y, -size.z);
+	glTexCoord2f(1, 1); glVertex3f(size.x, size.y, size.z);
+	glTexCoord2f(0, 1); glVertex3f(-size.x, size.y, size.z);
 	glEnd();
-	glTranslatef(-x, -y, -z);
+	glPopMatrix();
+}
+
+sf::Vector3f Shape::GetPosition() 
+{
+	return position;
+}
+
+sf::Vector3f Shape::GetSize() 
+{
+	return size;
 }
