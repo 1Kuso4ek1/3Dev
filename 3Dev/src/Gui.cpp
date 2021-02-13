67ddde81418bc98a6a7e62f9cd735c5cef7fda21
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-02-13 15:32:27 +0300

    Add files via upload

--- 3Dev/src/Gui.cpp
+++ 3Dev/src/Gui.cpp
@@ -1,6 +1,6 @@
 #include "Gui.h"
 
-int gui::Gui::CatchEvent(sf::Event& event, sf::RenderWindow& w)
+int gui::Gui::CatchEvent(sf::Event& event, sf::RenderWindow& w, bool checkTextBoxes)
 {
 	sf::Vector2i pixelPos = sf::Mouse::getPosition(w);
 	sf::Vector2f pos = w.mapPixelToCoords(pixelPos);
@@ -9,12 +9,23 @@ int gui::Gui::CatchEvent(sf::Event& event, sf::RenderWindow& w)
 			return buttons[i].ID;
 		}
 	}
+	if(checkTextBoxes) {
+		for (int i = 0; i < textboxes.size(); i++) {
+			textboxes[i].TextEntered(event);
+			textboxes[i].isPressed(event, pos);
+		}
+	}
 	return -1;
 }
 
-void gui::Gui::AddButton(gui::Button& button)
+void gui::Gui::Add(gui::Button& button)
 {
 	buttons.push_back(button);
+}
+
+void gui::Gui::Add(gui::TextBox& textbox)
+{
+	textboxes.emplace_back(textbox);
 }
 
 void gui::Gui::SetText(int ID, std::string text)
@@ -40,4 +51,15 @@ void gui::Gui::Draw(sf::RenderWindow& w)
 	for (int i = 0; i < buttons.size(); i++) {
 		buttons[i].Draw(w);
 	}
+	for (int i = 0; i < textboxes.size(); i++) {
+		textboxes[i].Draw(w);
+	}
+}
+
+std::string gui::Gui::GetTextBoxString(int id)
+{
+	for(auto i : textboxes) {
+		if(i.ID == id) return i.GetText();
+	}
+	return "";
 }
