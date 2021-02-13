e8e035e257e401026baf0c58676efcd4cdd51e2e
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-02-13 15:32:02 +0300

    Add files via upload

--- 3Dev/include/Gui.h
+++ 3Dev/include/Gui.h
@@ -1,18 +1,23 @@
 #pragma once
 #include "3Dev.h"
 #include "Button.h"
+#include "TextBox.h"
 namespace gui {
 	class Gui
 	{
 	public:
-		int CatchEvent(sf::Event& event, sf::RenderWindow& w);
+		int CatchEvent(sf::Event& event, sf::RenderWindow& w, bool checkTextBoxes = true);
 
-		void AddButton(gui::Button& button);
+		void Add(gui::Button& button);
+		void Add(gui::TextBox& textbox);
 		void SetText(int ID, std::string text);
 		void SetColor(int ID, sf::Color color);
 		void Draw(sf::RenderWindow& w);
+		
+		std::string GetTextBoxString(int id);
 	private:
 		std::vector<gui::Button> buttons;
+		std::vector<gui::TextBox> textboxes;
 	};
 }
 
