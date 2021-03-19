4b243848a5b6caf477642ba1a249b4c6fce9f991
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-03-19 18:44:09 +0300

    Third person camera implemented!

--- 3Dev/include/Camera.h
+++ 3Dev/include/Camera.h
@@ -8,6 +8,7 @@ public:
 	Camera(float x, float y, float z, float speed);
 
 	void Move(float time);
+	void ThirdPerson(float centerx, float centery, float centerz, float dist);
 	void Mouse(sf::RenderWindow& window);
 	void Look();
 	void Look(float x, float y, float z);
