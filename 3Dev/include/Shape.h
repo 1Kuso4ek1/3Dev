d6aab77f93246ac14a40b1badf2157874f3e8492
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-04-04 19:46:52 +0300

    Add files via upload

--- 3Dev/include/Shape.h
+++ 3Dev/include/Shape.h
@@ -4,8 +4,16 @@ class Shape {
 public:
 	Shape(float w, float h, float d, float x, float y, float z);
 
+	static void Draw(GLuint texture, float x, float y, float z, float w, float h, float d);
+	static void Draw(GLuint texture[6], float x, float y, float z, float w, float h, float d);
+
 	void Draw(GLuint texture);
 	void Draw(GLuint texture[6]);
+
+	void DrawPlane(GLuint texture);
+		
+	void SetPosition(float x, float y, float z);
+	void SetSize(float w, float h, float d);
 	
 	sf::Vector3f GetPosition();
 	sf::Vector3f GetSize();
