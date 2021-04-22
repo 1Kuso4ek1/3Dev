0a682fcbbb64467c6987e771502d11885e878611
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-04-22 15:19:28 +0300

    Cleared the Model class of garbage

--- 3Dev/src/Model.cpp
+++ 3Dev/src/Model.cpp
@@ -48,7 +48,7 @@ Model::~Model()
 
 bool Model::Load(std::string filename, std::string texture)
 {
-	ModelTexture = LoadTexture(texture);
+	ModelTexture = (texture == "") ? 0 : LoadTexture(texture);
 	Assimp::Importer importer;
 	const aiScene* scene = importer.ReadFile(filename, aiProcessPreset_TargetRealtime_Fast);
 	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE)
@@ -56,7 +56,7 @@ bool Model::Load(std::string filename, std::string texture)
 		std::cout << "ERROR::ASSIMP::" << importer.GetErrorString() << std::endl;
 		return false;
 	}
-	aiMesh* mesh = scene->mMeshes[0];
+	mesh = scene->mMeshes[0];
 	numVerts = mesh->mNumFaces * 3;
 	vertexArray = new float[mesh->mNumFaces * 3 * 3];
 	normalArray = new float[mesh->mNumFaces * 3 * 3];
@@ -87,92 +87,31 @@ bool Model::Load(std::string filename, std::string texture)
 	return true;
 }
 
-bool Model::Load(std::string filename)
+void Model::Draw()
 {
-	Assimp::Importer importer;
-	const aiScene* scene = importer.ReadFile(filename, aiProcess_Triangulate);
-	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE)
-	{
-		std::cout << "ERROR::ASSIMP::" << importer.GetErrorString() << std::endl;
-		return false;
-	}
-	aiMesh* mesh = scene->mMeshes[0];
-	numVerts = mesh->mNumFaces * 3;
-	vertexArray = new float[mesh->mNumFaces * 3 * 3];
-	normalArray = new float[mesh->mNumFaces * 3 * 3];
-	uvArray = new float[mesh->mNumFaces * 3 * 2];
-
-	for (unsigned int i = 0; i < mesh->mNumFaces; i++)
-	{
-		const aiFace& face = mesh->mFaces[i];
-
-		for (int j = 0; j < 3; j++)
-		{
-			aiVector3D uv = mesh->mTextureCoords[0][face.mIndices[j]];
-			memcpy(uvArray, &uv, sizeof(float) * 2);
-			uvArray += 2;
-
-			aiVector3D normal = mesh->mNormals[face.mIndices[j]];
-			memcpy(normalArray, &normal, sizeof(float) * 3);
-			normalArray += 3;
-
-			aiVector3D pos = mesh->mVertices[face.mIndices[j]];
-			memcpy(vertexArray, &pos, sizeof(float) * 3);
-			vertexArray += 3;
-		}
-	}
-
-	uvArray -= mesh->mNumFaces * 3 * 2;
-	normalArray -= mesh->mNumFaces * 3 * 3;
-	vertexArray -= mesh->mNumFaces * 3 * 3;
-	return true;
-}
-
-void Model::Draw(bool tex)
-{
-	if (tex)
-	{
-		glPushMatrix();
-		glTranslatef(position.x, position.y, position.z);
-		glScalef(size.x, size.y, size.z);
-		glRotatef(rotation.y, 0, 1, 0);
-		glRotatef(rotation.z, 0, 0, 1);
-		glRotatef(rotation.x, 1, 0, 0);
+	glPushMatrix();
+	glTranslatef(position.x, position.y, position.z);
+	glScalef(size.x, size.y, size.z);
+	glRotatef(rotation.y, 0, 1, 0);
+	glRotatef(rotation.z, 0, 0, 1);
+	glRotatef(rotation.x, 1, 0, 0);
 		
-		glBindTexture(GL_TEXTURE_2D, ModelTexture);
+	glBindTexture(GL_TEXTURE_2D, ModelTexture);
 
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glEnableClientState(GL_NORMAL_ARRAY);
-		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_NORMAL_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
-		glVertexPointer(3, GL_FLOAT, 0, vertexArray);
-		glNormalPointer(GL_FLOAT, 0, normalArray);
+	glVertexPointer(3, GL_FLOAT, 0, vertexArray);
+	glNormalPointer(GL_FLOAT, 0, normalArray);
 
-		glTexCoordPointer(2, GL_FLOAT, 0, uvArray);
+	glTexCoordPointer(2, GL_FLOAT, 0, uvArray);
 
-		glDrawArrays(GL_TRIANGLES, 0, numVerts);
-		glDisableClientState(GL_VERTEX_ARRAY);
-		glDisableClientState(GL_NORMAL_ARRAY);
-		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-		glPopMatrix();
-	}
-	else
-	{
-		glPushMatrix();
-		glTranslatef(position.x, position.y, position.z);
-		glScalef(size.x, size.y, size.z);
-	
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glEnableClientState(GL_NORMAL_ARRAY);
-
-		glVertexPointer(3, GL_FLOAT, 0, vertexArray);
-		glNormalPointer(GL_FLOAT, 0, normalArray);
-
-		glDrawArrays(GL_TRIANGLES, 0, numVerts);
-		glDisableClientState(GL_VERTEX_ARRAY);
-		glDisableClientState(GL_NORMAL_ARRAY);
-		glPopMatrix();
-	}
+	glDrawArrays(GL_TRIANGLES, 0, numVerts);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_NORMAL_ARRAY);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glPopMatrix();
 }
 
 void Model::Draw(GLuint texture)
@@ -183,7 +122,7 @@ void Model::Draw(GLuint texture)
 	glRotatef(rotation.y, 0, 1, 0);
 	glRotatef(rotation.z, 0, 0, 1);
 	glRotatef(rotation.x, 1, 0, 0);
-	
+		
 	glBindTexture(GL_TEXTURE_2D, texture);
 
 	glEnableClientState(GL_VERTEX_ARRAY);
