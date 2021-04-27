a39867f8f8478120cb4ade03b718b0e9c608aed8
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-04-27 21:18:55 +0300

    Add files via upload

--- 3Dev/include/Shape.h
+++ 3Dev/include/Shape.h
@@ -4,8 +4,8 @@ class Shape {
 public:
 	Shape(float w, float h, float d, float x, float y, float z);
 
-	static void Draw(GLuint texture, float x, float y, float z, float w, float h, float d);
-	static void Draw(GLuint texture[6], float x, float y, float z, float w, float h, float d);
+	static void Draw(GLuint texture, float x, float y, float z, float w, float h, float d, sf::Vector3f rotation = sf::Vector3f(0, 0, 0));
+	static void Draw(GLuint texture[6], float x, float y, float z, float w, float h, float d, sf::Vector3f rotation = sf::Vector3f(0, 0, 0));
 
 	void Draw(GLuint texture);
 	void Draw(GLuint texture[6]);
@@ -13,11 +13,14 @@ public:
 	void DrawPlane(GLuint texture);
 		
 	void SetPosition(float x, float y, float z);
+	void SetRotation(float rotationX, float rotationY, float rotationZ);
 	void SetSize(float w, float h, float d);
 	
 	sf::Vector3f GetPosition();
+	sf::Vector3f GetRotation();
 	sf::Vector3f GetSize();
 private:
 	sf::Vector3f size;
 	sf::Vector3f position;
+	sf::Vector3f rotation;
 };
