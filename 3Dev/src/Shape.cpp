4d4ce520d1b91ef137c317bc78c81899a45b114f
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-04-27 21:19:40 +0300

    Add files via upload

--- 3Dev/src/Shape.cpp
+++ 3Dev/src/Shape.cpp
@@ -1,11 +1,14 @@
 #include "Shape.h"
 
-Shape::Shape(float w, float h, float d, float x, float y, float z) : size(w, h, d), position(x, y, z) {}
+Shape::Shape(float w, float h, float d, float x, float y, float z) : size(w, h, d), position(x, y, z), rotation(0, 0, 0) {}
 
-void Shape::Draw(GLuint texture, float x, float y, float z, float w, float h, float d)
+void Shape::Draw(GLuint texture, float x, float y, float z, float w, float h, float d, sf::Vector3f rotation)
 {
 	glPushMatrix();
 	glTranslatef(x, y, z);
+	glRotatef(rotation.y, 0, 1, 0);
+	glRotatef(rotation.z, 0, 0, 1);
+	glRotatef(rotation.x, 1, 0, 0);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glBegin(GL_QUADS);
 	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, 1.0); glVertex3f(-w, -h, -d);
@@ -42,10 +45,13 @@ void Shape::Draw(GLuint texture, float x, float y, float z, float w, float h, fl
 	glPopMatrix();
 }
 
-void Shape::Draw(GLuint texture[6], float x, float y, float z, float w, float h, float d)
+void Shape::Draw(GLuint texture[6], float x, float y, float z, float w, float h, float d, sf::Vector3f rotation)
 {
 	glPushMatrix();
 	glTranslatef(x, y, z);
+	glRotatef(rotation.y, 0, 1, 0);
+	glRotatef(rotation.z, 0, 0, 1);
+	glRotatef(rotation.x, 1, 0, 0);
 	glBindTexture(GL_TEXTURE_2D, texture[0]);
 	glBegin(GL_QUADS);
 	glTexCoord2f(0, 0); glNormal3f(0.0, 0.0, 1.0); glVertex3f(-w, -h, -d);
@@ -98,17 +104,20 @@ void Shape::Draw(GLuint texture[6], float x, float y, float z, float w, float h,
 
 void Shape::Draw(GLuint texture) 
 {
-	Draw(texture, position.x, position.y, position.z, size.x, size.y, size.z);
+	Draw(texture, position.x, position.y, position.z, size.x, size.y, size.z, rotation);
 }
 
 void Shape::Draw(GLuint texture[6])
 {
-	Draw(texture, position.x, position.y, position.z, size.x, size.y, size.z);
+	Draw(texture, position.x, position.y, position.z, size.x, size.y, size.z, rotation);
 }
 
 void Shape::DrawPlane(GLuint texture) {
 	glPushMatrix();
 	glTranslatef(position.x, position.y, position.z);
+	glRotatef(rotation.y, 0, 1, 0);
+	glRotatef(rotation.z, 0, 0, 1);
+	glRotatef(rotation.x, 1, 0, 0);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glBegin(GL_QUADS);
 	
@@ -126,6 +135,11 @@ void Shape::SetPosition(float x, float y, float z)
 	position = sf::Vector3f(x, y, z);
 }
 
+void Shape::SetRotation(float rotationX, float rotationY, float rotationZ)
+{
+	rotation = sf::Vector3f(rotationX, rotationY, rotationZ);
+}
+
 void Shape::SetSize(float w, float h, float d)
 {
 	size = sf::Vector3f(w, h, d);
@@ -134,6 +148,11 @@ void Shape::SetSize(float w, float h, float d)
 sf::Vector3f Shape::GetPosition() 
 {
 	return position;
+}
+
+sf::Vector3f Shape::GetRotation() 
+{
+	return rotation;
 }
 
 sf::Vector3f Shape::GetSize() 
