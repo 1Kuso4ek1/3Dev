1d123b4c10c918e55f1cc3e86ee9494313946421
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-05-28 21:24:32 +0300

    Very accurate and completely unoptimized model-to-model collision

--- 3Dev/include/Physics.h
+++ 3Dev/include/Physics.h
@@ -5,12 +5,30 @@ std::pair<int, sf::Vector3f> collision(float x, float y, float z, Model& m, floa
 	for (int i = 0; i < m.numVerts * 3; i += 3) {
 		if(abs(x - ((m.GetPosition().x + m.vertexArray[i]))) <= p && abs(z - ((m.GetPosition().z + m.vertexArray[i + 2]))) <= p)
 		{
-			if (y <= (m.GetPosition().y + m.vertexArray[i + 1]) && y >= (m.GetPosition().y - m.vertexArray[i + 1]) / m.GetSize().y) {
+			if (y <= (m.GetPosition().y + m.vertexArray[i + 1]) && y >= (m.GetPosition().y - m.vertexArray[i + 1])) {
 				return std::make_pair(i, sf::Vector3f(((m.GetPosition().x + m.vertexArray[i])) - x, ((m.GetPosition().y + m.vertexArray[i + 1])) - y, ((m.GetPosition().z + m.vertexArray[i + 2])) - z));
 			}
 		}
 	}
 	return std::make_pair(-1, sf::Vector3f(0, 0, 0));
+}
+
+std::pair<std::vector<int>, std::vector<int>> collision(Model& m1, Model& m, float p = 1, int m1skip = 1, int mskip = 1)
+{
+	std::vector<int> v;
+	std::vector<int> v1;
+	for (int i = 0; i < m.numVerts * 3; i += 3 * mskip) {
+		for (int j = 0; j < m1.numVerts * 3; j += 3 * m1skip) {
+			if(fabsf(((m1.GetPosition().x + ((m1.vertexArray[j] * cos(m1.GetRotation().y * pi / 180)) * sin(m1.GetRotation().x * pi / 180)))) - ((m.GetPosition().x + m.vertexArray[i]))) <= p && fabsf(((m1.GetPosition().z + ((m1.vertexArray[j + 2] * cos(m1.GetRotation().y * pi / 180)) * cos(m1.GetRotation().z * pi / 180)))) - ((m.GetPosition().z + m.vertexArray[i + 2]))) <= p)
+			{
+				if ((m1.GetPosition().y + m1.vertexArray[j + 1]) <= (m.GetPosition().y + m.vertexArray[i + 1]) && (m1.GetPosition().y - m1.vertexArray[j + 1]) >= (m.GetPosition().y - m.vertexArray[i + 1])) {
+					v.push_back(j);
+					v1.push_back(i);
+				}
+			}
+		}
+	}
+	return std::make_pair(v, v1);
 }
 
 bool collision(Model& m, float x, float y, float z, float w = 0, float h = 0, float d = 0)
