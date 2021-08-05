e0723bf816cd675c690ad446ff4ad439b0c668a8
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-08-05 14:32:54 +0300

    Add files via upload

---
+++ 3Dev/src/Engine.cpp
@@ -0,0 +1,72 @@
+#include "Engine.h"
+
+void Engine::Init(float fov, float width, float height, float near, float far)
+{
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_ALPHA_TEST);
+    glAlphaFunc(GL_NOTEQUAL, 0);
+    glDepthMask(GL_TRUE);
+    glMatrixMode(GL_PROJECTION);
+    gluPerspective(fov, width / height, near, far);
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_NORMALIZE);
+    Light::EnableLighting();
+}
+
+void Engine::Init(std::function<void(void)> init)
+{
+    init();
+}
+
+void Engine::CreateWindow(float width, float height, std::string title, sf::Uint32 style)
+{
+    window.create(sf::VideoMode(width, height), title, style, settings);
+}
+
+void Engine::EventLoop(std::function<void(sf::Event&)> eloop)
+{
+    this->eloop = eloop;
+}
+
+void Engine::Loop(std::function<void(void)> loop)
+{
+    this->loop = loop;
+}
+
+void Engine::Launch()
+{
+    running = true;
+
+    while(running)
+    {
+        while(window.pollEvent(event))
+        {
+            eloop(event);
+        }
+
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        loop();
+
+        window.display();
+    }
+}
+
+void Engine::Close()
+{
+    running = false;
+}
+
+sf::RenderWindow& Engine::GetWindow()
+{
+    return window;
+}
+
+sf::ContextSettings& Engine::GetSettings()
+{
+    return settings;
+}
